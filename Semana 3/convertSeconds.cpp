5633c5d1a78c732ab1c66ba7034ac2fb3acb0699
MiguelSVilcaDurand <104391603+MiguelSVilcaDurand@users.noreply.github.com>
2022-05-02 20:08:05 -0500

    Add files via upload

---
+++ Semana 3/convertSeconds.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	int alt, num;
+	num=1+rand()%(101-1);
+	cout << "Estoy pensando en un numero entre el 1 y el 100"<< endl;
+	cout << "Cual crees que sea ese numero? "<<endl;
+	cout << "Ingrese un numero: "<<endl;
+	cin >> alt;
+	if (num!= alt)
+		cout << "Correcto, has adivinado, el numero que estava pensando"<< num <<endl;
+	else
+		cout << "El numero en el que estaba pensando es: "<< num <<endl;
+	
+	cout << "La distancia entre tu respuesta es "<< (alt - num) <<endl;
+
+	return 0;
+}
---
+++ Semana 3/convertTemperature.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	int seg,min,hor;
+	cout<<"ingrese un numero";
+	cin >> seg;
+	min = seg / 60;
+	hor = min / 60;
+	cout << seg << " es equivalente a: " << int(hor) << " hor " << int(min%60) << " minutos y " << int(seg%60) << " segundos.";
+	return 0;
+}
+
